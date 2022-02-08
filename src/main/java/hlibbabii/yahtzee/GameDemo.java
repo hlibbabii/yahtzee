9c1f235ad4b962be3a33b0521f8ceb5edce7ccd1
hlib <hlibbabii@gmail.com>
2022-02-08 16:52:43 +0100

    rename variable

--- src/main/java/hlibbabii/yahtzee/GameDemo.java
+++ src/main/java/hlibbabii/yahtzee/GameDemo.java
@@ -9,9 +9,9 @@ public class GameDemo {
         Player player1 = new DummyPlayer();
         Player player2 = new DummyPlayer();
 
-        Game game = new Game(player1, player2);
+        Game yahtzeeGame = new Game(player1, player2);
 
-        GameStats gameStats = game.play();
+        GameStats gameStats = yahtzeeGame.play();
         System.out.println("Player1");
         System.out.println(gameStats.getPlayerStats(player1));
         System.out.println("Player2");
